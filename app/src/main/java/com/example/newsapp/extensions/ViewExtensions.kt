dd68f77727f32034e8e89fa0c479b75709dc1629
David Martins <david.martins@farfetch.com>
2019-10-14 17:04:13 +0100

    Update ViewExtensions

--- app/src/main/java/com/example/newsapp/extensions/ViewExtensions.kt
+++ app/src/main/java/com/example/newsapp/extensions/ViewExtensions.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 fun View.gone() {
     this.visibility = View.GONE
@@ -22,5 +23,12 @@ fun AppCompatActivity.hideKeyboard() {
     if (view != null) {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
+
+fun Fragment.hideKeyboard() {
+    val activity = this.activity
+    if (activity is AppCompatActivity) {
+        activity.hideKeyboard()
     }
 }
